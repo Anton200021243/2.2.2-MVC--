f8029e95ecaf72f837240e7981e08ab367497f69
Anton200021243 <mortal0011@gmail.com>
2024-10-22 22:48:03 +0300

    make controller to get

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,11 +1,26 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 
 @Controller
 @RequestMapping("/car")
 public class CarController {
+    CarService carService;
 
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public String index(@RequestParam("count") int count, Model model) {
+        model.addAttribute("cars", carService.getCars(count));
+        return "/cars";
+    }
 }
